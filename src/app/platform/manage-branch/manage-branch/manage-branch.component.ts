80f31bca7c352363e98f5fc4d447bac7587589a3
Sumbalbatool <Sumbalbatool@vizzweb.com>
2024-06-27 15:25:34 +0500

    working in companies component

--- src/app/platform/manage-branch/manage-branch/manage-branch.component.ts
+++ src/app/platform/manage-branch/manage-branch/manage-branch.component.ts
@@ -34,310 +34,254 @@ declare let $: any;
   imports: [CommonModule, RouterModule, FormsModule, LazyLoadImageModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class ManageBranchComponent implements OnInit, AfterViewInit {
-  @ViewChildren('carouselElement') carouselElements: QueryList<any> =
-    new QueryList();
+export class ManageBranchComponent implements OnInit {
+  pageSize: number = 10;
   currentPage: number = 1;
-  itemsPerPage: number = 4;
-  showDropdown: boolean = false;
+  totalPages!: number;
   searchQuery: string = '';
-  branchId: string | null = null;
-  newData: any = null;
-  branches: any[] = [];
-  images: any;
-  selectedBranch: any;
-  selectedPriceRange: string = '';
-  filterVisible: boolean = false;
-  menuCategories: any[] = [];
-  sortOption: string = 'All';
-  filteredCategories: any[] = [];
-  defaultImage = '../../../assets/halls/image.webp';
-  isDropdownOpen: boolean = false;
-  activeIndex: number = 0;
-  activeDropdownIndex: number | null = null;
-  onSearchChange() {
-    if (!this.searchQuery) {
-      this.filteredCategories = [...this.menuCategories];
-      this.getBranchData(); // Call getBranchData function when search query is null
-      return;
-    }
-    this.filteredCategories = this.menuCategories.filter((category) =>
-      category.branch_name
-        .toLowerCase()
-        .includes(this.searchQuery.toLowerCase())
+  pages!: number[];
+  paginatedData!: any[];
+  filteredData: any[] = [];
+  constructor(private router: Router) {}
+  sortProductsDesc(): void {
+    this.tableData = this.tableData.sort((a, b) =>
+      b.customer_name.localeCompare(a.customer_name)
     );
   }
 
-  filterByAlphabet(alphabet: string) {
-    if (alphabet === 'all') {
-      this.filteredCategories = [...this.menuCategories]; // Reset to all categories
-    } else {
-      // Filter categories that start with the selected alphabet
-      this.filteredCategories = this.menuCategories.filter((category) =>
-        category.branch_name.toLowerCase().startsWith(alphabet.toLowerCase())
-      );
-    }
-    this.searchQuery = '';
-    this.showDropdown = false;
-    this.getBranchData();
+  // Example event handler for a button click
+  onSortButtonClick(): void {
+    this.sortProductsDesc();
+    console.log(1255);
   }
-  constructor(
-    private router: Router,
-    private apiService: ApiService,
-    private manageBranchService: ManageBranchService
-  ) {}
+  tableData = [
+    {
+      id: 1,
+      order_id: 'Mark',
+      customer_name: 'ahmad',
+      date_time: '@mdo',
+      booking_date: '2 feb 2023',
+      hall_name: 'A1+A2+A3',
+      order_amount: '200,000',
+      advance: '20,000',
+    },
+    {
+      id: 2,
+      order_id: 'Mark',
+      customer_name: 'Otto',
+      date_time: '@mdo',
+      booking_date: '2 feb 2023',
+      hall_name: 'A1+A2+A3',
+      order_amount: '200,000',
+      advance: '20,000',
+    },
+    {
+      id: 3,
+      order_id: 'Mark',
+      customer_name: 'Otto',
+      date_time: '@mdo',
+      booking_date: '2 feb 2023',
+      hall_name: 'A1+A2+A3',
+      order_amount: '200,000',
+      advance: '20,000',
+    },
+    {
+      id: 4,
+      order_id: 'Mark',
+      customer_name: 'Otto',
+      date_time: '@mdo',
+      booking_date: '2 feb 2023',
+      hall_name: 'A1+A2+A3',
+      order_amount: '200,000',
+      advance: '20,000',
+    },
+    {
+      id: 5,
+      order_id: 'Mark',
+      customer_name: 'Otto',
+      date_time: '@mdo',
+      booking_date: '2 feb 2023',
+      hall_name: 'A1+A2+A3',
+      order_amount: '200,000',
+      advance: '20,000',
+    },
+    {
+      id: 6,
+      order_id: 'Mark',
+      customer_name: 'Otto',
+      date_time: '@mdo',
+      booking_date: '2 feb 2023',
+      hall_name: 'A1+A2+A3',
+      order_amount: '200,000',
+      advance: '20,000',
+    },
+    {
+      id: 7,
+      order_id: 'Mark',
+      customer_name: 'Otto',
+      date_time: '@mdo',
+      booking_date: '2 feb 2023',
+      hall_name: 'A1+A2+A3',
+      order_amount: '200,000',
+      advance: '20,000',
+    },
+    {
+      id: 8,
+      order_id: 'Mark',
+      customer_name: 'Otto',
+      date_time: '@mdo',
+      booking_date: '2 feb 2023',
+      hall_name: 'A1+A2+A3',
+      order_amount: '200,000',
+      advance: '20,000',
+    },
+    {
+      id: 9,
+      order_id: 'Mark',
+      customer_name: 'Otto',
+      date_time: '@mdo',
+      booking_date: '2 feb 2023',
+      hall_name: 'A1+A2+A3',
+      order_amount: '200,000',
+      advance: '20,000',
+    },
+    {
+      id: 10,
+      order_id: 'Mark',
+      customer_name: 'Otto',
+      date_time: '@mdo',
+      booking_date: '2 feb 2023',
+      hall_name: 'A1+A2+A3',
+      order_amount: '200,000',
+      advance: '20,000',
+    },
+    {
+      id: 11,
+      order_id: 'Mark',
+      customer_name: 'Otto',
+      date_time: '@mdo',
+      booking_date: '2 feb 2023',
+      hall_name: 'A1+A2+A3',
+      order_amount: '200,000',
+      advance: '20,000',
+    },
+    {
+      id: 12,
+      order_id: 'Mark',
+      customer_name: 'Otto',
+      date_time: '@mdo',
+      booking_date: '2 feb 2023',
+      hall_name: 'A1+A2+A3',
+      order_amount: '200,000',
+      advance: '20,000',
+    },
+    {
+      id: 13,
+      order_id: 'Mark',
+      customer_name: 'Otto',
+      date_time: '@mdo',
+      booking_date: '2 feb 2023',
+      hall_name: 'A1+A2+A3',
+      order_amount: '200,000',
+      advance: '20,000',
+    },
+    {
+      id: 14,
+      order_id: 'Mark',
+      customer_name: 'Otto',
+      date_time: '@mdo',
+      booking_date: '2 feb 2023',
+      hall_name: 'A1+A2+A3',
+      order_amount: '200,000',
+      advance: '20,000',
+    },
+    {
+      id: 15,
+      order_id: 'Mark',
+      customer_name: 'Otto',
+      date_time: '@mdo',
+      booking_date: '2 feb 2023',
+      hall_name: 'A1+A2+A3',
+      order_amount: '200,000',
+      advance: '20,000',
+    },
+    {
+      id: 16,
+      order_id: 'Mark',
+      customer_name: 'Otto',
+      date_time: '@mdo',
+      booking_date: '2 feb 2023',
+      hall_name: 'A1+A2+A3',
+      order_amount: '200,000',
+      advance: '20,000',
+    },
+  ];
 
+  openNewComponent() {
+    this.router.navigate(['view-manage-customer']);
+    console.log('call function');
+  }
   ngOnInit(): void {
-    this.getBranchData();
-    this.getAllBranches();
+    this.calculatePages();
+    this.filterData();
   }
-  getBranchData() {
-    let url = new URL(`${environment.baseURL}/api/hall/branch/branches`);
-    this.apiService
-      .get(url.href)
-      .pipe(first())
-      .subscribe(
-        (response) => {
-          // this.menuCategories = response?.branches;
-          this.menuCategories = response.branches.map((branch: any) => ({
-            ...branch,
-            truncatedAddress: this.getTruncatedAddress(
-              branch.address[0].street_adress
-            ),
-          }));
-          this.filteredCategories = [...this.menuCategories];
-          this.paginateData();
-        },
-        (error) => {
-          console.log(error);
-        }
-      );
+  selectedButton: string = 'all';
+
+  selectButton(button: string) {
+    this.selectedButton = button;
   }
-  getTruncatedAddress(address: string): string {
-    if (!address) return '';
-    const words = address.split(' ');
-    if (words.length <= 3) return address;
-    return words.slice(0, 3).join(' ') + '...';
-  }
-  deleteBranch(branchId: string, index: number) {
-    const url = `https://dev-backend.hamaravenue.com/api/hall/branch/branch/${branchId}`;
-
-    Swal.fire({
-      title: 'Are you sure you want to delete it?',
-
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#d33',
-      cancelButtonColor: 'grey',
-      confirmButtonText: 'Yes',
-    }).then((result) => {
-      if (result.isConfirmed) {
-        this.apiService
-          .delete(url)
-          .pipe(first())
-          .subscribe(
-            () => {
-              Swal.fire({
-                title: 'Deleted!',
-                text: 'Branch has been deleted.',
-                icon: 'success',
-              });
-              this.filteredCategories = this.filteredCategories.filter(
-                (branch) => branch._id !== branchId
-              );
-              if (this.activeDropdownIndex === index) {
-                this.activeDropdownIndex = null; // Close the dropdown
-              }
-              this.getBranchData();
-              this.getAllBranches();
-              this.paginateData();
-            },
-            (error) => {
-              // Error occurred during deletion, display error message
-              Swal.fire({
-                title: 'Error!',
-                text: 'An error occurred while deleting the branch.',
-                icon: 'error',
-              });
-
-              console.error('Error deleting branch:', error);
-            }
-          );
-      }
-    });
+  // Calculate total number of pages and set paginated data
+  calculatePages(): void {
+    this.totalPages = Math.ceil(this.tableData.length / this.pageSize);
+    this.paginate();
   }
 
-  // toggleDropdown(index: number) {
-  //   this.activeDropdownIndex =
-  //     this.activeDropdownIndex === index ? null : index;
-  // }
-  toggleDropdown(index: number) {
-    if (this.activeDropdownIndex === index) {
-      this.activeDropdownIndex = null; // Close the dropdown
-    } else {
-      this.activeDropdownIndex = index; // Open the dropdown
-    }
+  // Paginate data based on current page
+  paginate(): void {
+    const startIndex = (this.currentPage - 1) * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    this.paginatedData = this.tableData.slice(startIndex, endIndex);
+    this.pages = Array(this.totalPages)
+      .fill(0)
+      .map((x, i) => i + 1);
   }
 
-  navigateToAddBranch() {
-    this.router.navigate(['/branch-manage/add-branch']);
-  }
-
-  setActiveIndex(index: number) {
-    this.activeIndex = index;
-  }
-
-  getCardDetails(id: string) {
-    this.apiService
-      .get(`https://dev-backend.hamaravenue.com/api/hall/branch/branches/${id}`)
-      .subscribe(
-        (response: any) => {
-          console.log('Card Details:', response);
-        },
-        (error: any) => {
-          console.error('Error fetching card details:', error);
-        }
-      );
-  }
-
-  editBranch(branchId: string) {
-    console.log('branch-id', branchId);
-    this.router.navigate(['/branch-manage/edit-branch', branchId]);
-  }
-  viewBranch(branchId: string) {
-    this.router.navigate(['/branch-manage/branch-detail', branchId]);
-  }
-  paginateData(): void {
-    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
-    const endIndex = startIndex + this.itemsPerPage;
-    this.filteredCategories = this.menuCategories.slice(startIndex, endIndex);
-
-    // If the current page has no items due to deletion, adjust the current page
-    if (this.filteredCategories.length === 0 && this.currentPage > 1) {
-      this.currentPage--; // Move to the previous page
-      this.paginateData(); // Recalculate pagination with the adjusted page
-    }
-  }
-
-  get totalPages(): number {
-    return Math.ceil(this.menuCategories.length / this.itemsPerPage);
-  }
-
-  get pages(): number[] {
-    const pageCount = Math.min(5, this.totalPages);
-    const startPage = Math.max(1, this.currentPage - Math.floor(pageCount / 2));
-    const endPage = Math.min(this.totalPages, startPage + pageCount - 1);
-    return Array.from(
-      { length: endPage - startPage + 1 },
-      (_, i) => startPage + i
-    );
-  }
-
+  // Set current page
   setCurrentPage(page: number): void {
-    if (page >= 1 && page <= this.totalPages) {
-      this.currentPage = page;
-      this.paginateData();
-    }
+    this.currentPage = page;
+    this.paginate();
   }
 
-  nextPage(): void {
-    if (this.currentPage < this.totalPages) {
-      this.currentPage++;
-      window.scrollTo({
-        top: 0,
-        behavior: 'smooth',
-      });
-      this.paginateData();
-    }
-  }
-
+  // Go to previous page
   prevPage(): void {
     if (this.currentPage > 1) {
       this.currentPage--;
-      this.paginateData();
+      this.paginate();
     }
   }
 
-  ngAfterViewInit(): void {
-    this.carouselElements.changes.subscribe(() => {
-      this.carouselElements.forEach((carousel) => {
-        $(carousel.nativeElement).carousel();
-      });
+  // Go to next page
+  nextPage(): void {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.paginate();
+    }
+  }
+
+  filterData(): void {
+    this.filteredData = this.tableData.filter((item: any) => {
+      // Use optional chaining to safely access name and email properties
+      return (
+        item.name?.toLowerCase()?.includes(this.searchQuery.toLowerCase()) ||
+        false ||
+        item.email?.toLowerCase()?.includes(this.searchQuery.toLowerCase()) ||
+        false
+      );
     });
-  }
-  //filter code
-
-  toggleFilter() {
-    this.filterVisible = !this.filterVisible;
+    this.calculatePages(); // Recalculate pagination after filtering
   }
 
-  closeFilters() {
-    this.filterVisible = false;
-    this.selectedBranch = '';
-    this.getBranchData();
-  }
-  sortBy(option: string) {
-    if (option === 'ascending') {
-      this.menuCategories.sort((a, b) =>
-        a.branch_name && b.branch_name
-          ? a.branch_name.localeCompare(b.branch_name)
-          : 0
-      );
-      this.sortOption = 'Ascending';
-    } else if (option === 'descending') {
-      this.menuCategories.sort((a, b) =>
-        a.branch_name && b.branch_name
-          ? b.branch_name.localeCompare(a.branch_name)
-          : 0
-      );
-      this.sortOption = 'Descending';
-    } else if (option === 'all') {
-      // Display the "All" option when sorting by both ascending and descending
-      this.menuCategories.sort((a, b) =>
-        a.branch_name && b.branch_name
-          ? a.branch_name.localeCompare(b.branch_name)
-          : 0
-      );
-      this.sortOption = 'All';
-    }
-
-    this.showDropdown = false;
-    this.paginateData();
-  }
-
-  toggleSorting() {
-    this.showDropdown = !this.showDropdown;
-    console.log('sorting');
-  }
-  //filter branches
-
-  getAllBranches() {
-    let url = `${environment.baseURL}/api/hall/branch/branches/select`;
-
-    this.apiService
-      .get(url)
-      .pipe(first())
-      .subscribe(
-        (response: any) => {
-          this.branches = response.branches.map((branch: any) => ({
-            _id: branch._id,
-            branch_name: branch.branch_name,
-          }));
-          console.log('branch', this.branches);
-        },
-        (error) => {
-          console.log(error);
-        }
-      );
-  }
-
-  filterByBranch() {
-    if (this.selectedBranch) {
-      this.filteredCategories = this.menuCategories.filter(
-        (card) => card._id === this.selectedBranch
-      );
-    } else {
-      this.filteredCategories = this.menuCategories;
-    }
+  onSearchChange(): void {
+    this.filterData();
   }
 }
